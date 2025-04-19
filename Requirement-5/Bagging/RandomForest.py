231af382324314eeec4832a17077bc209eb31bd7
asmaa-204 <asmaa.abozaid204@gmail.com>
2025-04-19 18:40:32 +0200

    finished bagging

--- Requirement-5/Bagging/RandomForest.py
+++ Requirement-5/Bagging/RandomForest.py
@@ -3,7 +3,8 @@ from typing import Optional
 from sklearn.tree import DecisionTreeClassifier
 from Bagging import Bagging
 
-class RandomForestClassifier(Bagging):  
+
+class RandomForestClassifier(Bagging):
     def __init__(
         self,
         # Bagging Hyperparameters
@@ -24,20 +25,27 @@ class RandomForestClassifier(Bagging):
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.random_state = random_state
-        
+
         # TODO 1.0: Create a new DecisionTreeClassifier and pass to it the relevant hyperparameters (from self)
-        # TODO 1.1: Which of the features passed is responsible for column subsampling?
-        self.base_model: DecisionTreeClassifier = None
-        
+        # TODO 1.1: Which of the features passed is responsible for column subsampling? "max_features"
+        self.base_model : DecisionTreeClassifier = DecisionTreeClassifier(
+            max_depth=self.max_depth,
+            max_features=self.max_features,
+            min_samples_split=self.min_samples_split,
+            random_state=self.random_state,
+        )
+
         # TODO 2: Initialize the Bagging base class (from self)
-        super().__init__(None)
-    
-    # ✅ Random Forest Implementation is done here. Go back to Ensemble.ipynb for a quick test and some analysis.
-    
+        super().__init__(
+            model=self.base_model,
+            n_estimators=self.n_estimators,
+            max_samples=self.max_samples,
+            random_state=self.random_state,
+        )
+
     # Ignore this function. It allows setting parameters for debugging or visualization.
     def set_params(self, **params):
         # Get the current parameters
-        current_params = self.__dict__.copy()        
-        current_params.update(params)        
+        current_params = self.__dict__.copy()
+        current_params.update(params)
         self.__init__(**current_params)
-        
